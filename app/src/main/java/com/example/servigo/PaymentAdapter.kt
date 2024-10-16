e2deac164348e9ed38038d548b7d41519be9ed55
Kent Abiatar <kentabiatar@gmail.com>
2024-10-16 19:54:27 +0700

    added payment page (recycler dont have data yet)

---
+++ app/src/main/java/com/example/servigo/PaymentAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.servigo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.servigo.HistoryAdapter.HistoryViewHolder
+
+class PaymentAdapter(private val paymentList : ArrayList<paymentData>) : RecyclerView.Adapter<PaymentAdapter.PaymentViewHolder>() {
+
+    class PaymentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val payitem: TextView = itemView.findViewById(R.id.payItem)
+        val payDesc: TextView = itemView.findViewById(R.id.payDesc)
+        val payAmount: TextView = itemView.findViewById(R.id.payAmount)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaymentViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.payment_list_recycler, parent, false)
+        return PaymentViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return paymentList.size
+    }
+
+    override fun onBindViewHolder(holder: PaymentViewHolder, position: Int) {
+        val paymentItem = paymentList[position]
+        holder.payitem.text = paymentItem.item
+        holder.payDesc.text = paymentItem.desc
+        holder.payAmount.text = paymentItem.amount
+    }
+
+
+}
---
+++ app/src/main/java/com/example/servigo/paymentData.kt
@@ -0,0 +1,8 @@
+package com.example.servigo
+
+data class paymentData(
+    val item: String,
+    val desc: String,
+    val amount: String
+
+)
